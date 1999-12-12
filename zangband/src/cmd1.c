ead02b3b7eb41448d6d363d7baaf16f88b98c56f
sfuerst <>
1999-12-12 05:58:08 +0000

    The critical hit code now does not display a message if nothing happens. This stops the double "you hit x" messages.

--- zangband/src/cmd1.c
+++ zangband/src/cmd1.c
@@ -205,7 +205,6 @@ static sint critical_melee(int chance, int sleeping_bonus, char m_name[], object
 	else
 	{
 		mult_m_crit = 10;
-		msg_format("You hit %s.", m_name);
 	}
 
 	return (mult_m_crit);
@@ -1540,10 +1539,14 @@ void py_attack(int y, int x)
 	bonus = p_ptr->to_h + o_ptr->to_h;
 	chance = (p_ptr->skill_thn + (bonus * BTH_PLUS_ADJ));
 
+	msg_format("Total number of blows %d", blows);
+
 
 	/* Attack once for each legal blow */
 	while (num++ < blows)
 	{
+		
+		msg_format("num blows %d", num);
 		/* Test for hit */
 		if (test_hit_combat(chance+ sleeping_bonus, r_ptr->ac+ terrain_bonus, m_ptr->ml))
 		{
