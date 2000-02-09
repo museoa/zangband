367abeb20f61af60b847257d2f7a4cbf81780efa
sfuerst <>
2000-02-09 12:00:16 +0000

    Rescaled monk attacks a bit.  Hopefully this isn't quite so powerful at the start.

--- zangband/src/xtra1.c
+++ zangband/src/xtra1.c
@@ -3484,13 +3484,13 @@ void calc_bonuses(void)
 	else if (p_ptr->pclass == CLASS_MONK && monk_empty_hands())
 	{
 		p_ptr->num_blow = 2;
-
-		if (p_ptr->lev >  4) p_ptr->num_blow++;
+		
 		if (p_ptr->lev > 9) p_ptr->num_blow++;
 		if (p_ptr->lev > 14) p_ptr->num_blow++;
 		if (p_ptr->lev > 24) p_ptr->num_blow++;
 		if (p_ptr->lev > 34) p_ptr->num_blow++;
 		if (p_ptr->lev > 44) p_ptr->num_blow++;
+		if (p_ptr->lev > 49) p_ptr->num_blow++;
 
 		if (monk_heavy_armor()) p_ptr->num_blow /= 2;
 
