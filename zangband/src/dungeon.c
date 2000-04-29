63ca321f5388df44cdeacc2aff47e26e870f1a0a
sfuerst <>
2000-04-29 11:14:41 +0000

    Fixed merge problems with dungeon.c

--- zangband/src/dungeon.c
+++ zangband/src/dungeon.c
@@ -4144,29 +4144,6 @@ void play_game(bool new_game)
 			}
 		}
 	}
-<<<<<<< dungeon.c
-=======
-
-	/* Hack - if note file exists, load it */
-	if (!new_game && take_notes)
-	{
-		char long_day[30];
-		time_t ct = time((time_t*)NULL);
-		FILE *fff;
-
-		/* Open file */
-		fff = my_fopen(notes_file(), "a");
-
-		/* Get the date */
-		strftime(long_day, 30, "%Y-%m-%d at %H:%M:%S", localtime(&ct));
-
-		/* Add in continuation info */
-		fprintf(fff, "================================================\n");
-		fprintf(fff, "New session start: %s\n\n", long_day);
-
-		/* Close file */
-		my_fclose(fff);
-	}
 
 	/* Roll new character */
 	if (new_game)
@@ -4213,25 +4190,24 @@ void play_game(bool new_game)
 	}
 	
 	/* Hack - if note file exists, load it */
-	if (!new_game && take_notes) {
-
-   	  char long_day[30];
- 	  time_t ct = time((time_t*)NULL);
-	  FILE *fff;
+	if (!new_game && take_notes)
+	{
+		char long_day[30];
+		time_t ct = time((time_t*)NULL);
+		FILE *fff;
  
-  	  /* Open file */
-          fff = my_fopen(notes_file(), "a");
+		/* Open file */
+		fff = my_fopen(notes_file(), "a");
   
-  	  /* Get the date */
-  	  strftime(long_day, 30, "%Y-%m-%d at %H:%M:%S", localtime(&ct));
+		/* Get the date */
+		strftime(long_day, 30, "%Y-%m-%d at %H:%M:%S", localtime(&ct));
   
-  	  /* Add in continuation info */
-  	  fprintf(fff, "================================================\n");
-  	  fprintf(fff, "New session start: %s\n\n", long_day);
+		/* Add in continuation info */
+		fprintf(fff, "================================================\n");
+		fprintf(fff, "New session start: %s\n\n", long_day);
   
-          /* Close file */
-          my_fclose(fff);
-  	  
+		/* Close file */
+		my_fclose(fff);
 	}
 
 	/* Flash a message */
