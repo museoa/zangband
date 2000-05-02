3fd835e69a21e54b1c8a7e9382677abf41908d16
rr9 <>
2000-05-02 17:05:48 +0000

    PyZ: Added code for racial activations.

--- zangband/lib/script/base/skill.py
+++ zangband/lib/script/base/skill.py
@@ -16,6 +16,7 @@ class skill:
 	name = "abstract skill"
 	level = 0
 	mana = 0
+	cost = 0
 	fail = 0
 	info = ""
 
@@ -25,6 +26,83 @@ class skill:
 	# Use the skill
 	def use(self):
 		pass
+
+
+#####################################################################
+# Base class for racial powers
+#####################################################################
+class racial_power(skill):
+	name = "abstract racial power"
+	active = 1
+	difficulty = 0
+	use_stat = 0
+
+	def get_difficulty(self):
+		return self.difficulty
+
+	def use(self):
+		from variable import player
+		from angband.io import msg_print, get_check, PR_HP, PR_MANA, PW_PLAYER, PW_SPELL
+		from angband.random import randint
+		from variable import ui
+		from angband.spells import take_hit
+
+		# Not enough mana - use hp
+		if player.csp < self.mana:
+			use_hp = 1
+		else:
+			use_hp = 0
+
+		# Power is not available yet
+		if player.level < self.level:
+			msg_print("You need to attain level %d to use this power." % (self.level))
+			energy_use = 0
+			return 0
+		# Too confused
+		elif player.confused:
+			msg_print("You are too confused to use this power.")
+			energy_use = 0
+			return 0
+		# Risk death?
+		elif (use_hp and (player.chp < self.cost)):
+			if not get_check("Really use the power in your weakened state? "):
+				energy_use = 0
+				return 0
+
+		# Else attempt to do it!
+
+		difficulty = self.get_difficulty()
+
+		if player.stun:
+			difficulty = difficulty + player.stun
+		elif player.level > self.level:
+			lev_adj = ((player.level - self.level) / 3)
+			if (lev_adj > 10): lev_adj = 10
+			difficulty = difficulty - lev_adj
+
+		if difficulty < 5: difficulty = 5
+
+		# take time and pay the price
+		energy_use = 100
+
+		if use_hp:
+			take_hit((self.cost / 2) + randint(self.cost / 2), "concentrating too hard")
+		else:
+			player.csp = player.csp - ((self.cost / 2) + randint(self.cost / 2))
+
+		# Redraw mana and hp
+		player.redraw = player.redraw & (PR_HP | PR_MANA)
+
+		# Window stuff
+		player.window = player.window & (PW_PLAYER | PW_SPELL)
+
+		# Success?
+		if randint(player.stat_cur_get(self.use_stat)) >= ((difficulty / 2) + randint(difficulty / 2)):
+			self.effect()
+			return
+
+		msg_print("You've failed to concentrate hard enough.")
+		return 0
 
 
 
