471b1ac1b042869eee9e786c1b24dd55ba8d24db
rr9 <>
2000-05-02 17:12:57 +0000

    Moved the player-races to the data folder.

---
+++ zangband/lib/script/worlds/nexus/player/race/default.py
@@ -0,0 +1,65 @@
+# Make the races known
+from base.birth import player_race_birth
+
+from data.player.race import amberite
+player_race_birth.races.append(amberite.amberite)
+
+from data.player.race import barbaria
+player_race_birth.races.append(barbaria.barbarian)
+
+from data.player.race import beastman
+player_race_birth.races.append(beastman.beastman)
+
+from data.player.race import cyclops
+player_race_birth.races.append(cyclops.cyclops)
+
+from data.player.race import dark_elf
+player_race_birth.races.append(dark_elf.dark_elf)
+
+from data.player.race import draconia
+player_race_birth.races.append(draconia.draconian)
+
+from data.player.race import dwarf
+player_race_birth.races.append(dwarf.dwarf)
+
+from data.player.race import elf
+player_race_birth.races.append(elf.elf)
+
+from data.player.race import gnome
+player_race_birth.races.append(gnome.gnome)
+
+from data.player.race import golem
+player_race_birth.races.append(golem.golem)
+
+from data.player.race import h_giant
+player_race_birth.races.append(h_giant.half_giant)
+
+from data.player.race import h_ogre
+player_race_birth.races.append(h_ogre.half_ogre)
+
+from data.player.race import h_titan
+player_race_birth.races.append(h_titan.half_titan)
+
+from data.player.race import h_troll
+player_race_birth.races.append(h_troll.half_troll)
+
+from data.player.race import half_elf
+player_race_birth.races.append(half_elf.half_elf)
+
+from data.player.race import half_orc
+player_race_birth.races.append(half_orc.half_orc)
+
+from data.player.race import high_elf
+player_race_birth.races.append(high_elf.high_elf)
+
+from data.player.race import hobbit
+player_race_birth.races.append(hobbit.hobbit)
+
+from data.player.race import human
+player_race_birth.races.append(human.human)
+
+from data.player.race import imp
+player_race_birth.races.append(imp.imp)
+
+# XXX
+
