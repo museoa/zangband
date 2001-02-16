8619eb8e6bdf2f10a9aa9a4f00754a55d1860cf2
rr9 <>
2001-02-16 15:31:40 +0000

    Changed the version number to 2.5.4. Removed a duplicated flag in the artifact definitions.

--- zangband/src/defines.h
+++ zangband/src/defines.h
@@ -35,7 +35,7 @@
 /*
  * Current version string
  */
-#define VERSION_STRING	"2.5.3"
+#define VERSION_STRING	"2.5.4"
 
 
 /*
@@ -57,7 +57,7 @@
 #define FAKE_VERSION   0
 #define FAKE_VER_MAJOR 2
 #define FAKE_VER_MINOR 5
-#define FAKE_VER_PATCH 3
+#define FAKE_VER_PATCH 4
 #endif /* USE_SCRIPT */
 
 #define ANGBAND_2_8_1
