8b78947ca55b0b4dc07d56a9c86cdc9cb132170f
rr9 <>
2001-02-28 17:30:55 +0000

    Added the classes for the player's sex.

---
+++ zangband/lib/script/data/player/gender/female.py
@@ -0,0 +1,7 @@
+from base.p_sex import player_sex
+
+class female(player_sex):
+	title = "Female"
+	winner = "Queen"
+	number = 0
+
---
+++ zangband/lib/script/data/player/gender/male.py
@@ -0,0 +1,7 @@
+from base.p_sex import player_sex
+
+class male(player_sex):
+	title = "Male"
+	winner = "King"
+	number = 1
+
