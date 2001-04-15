d944b437e936229fd315344d7d651ab2e7d48e58
sfuerst <>
2001-04-15 11:32:03 +0000

    Added the y/n/k prompt to the easy_floor option, when there is only one object to pick up.

--- zangband/src/cmd1.c
+++ zangband/src/cmd1.c
@@ -699,60 +699,6 @@ void py_pickup_aux(int o_idx)
 }
 
 
-#if 0
-
-/*
- * Automatically destroy items in this grid.
- */
-static void auto_destroy_items(cave_type *c_ptr)
-{
-	s16b this_o_idx, next_o_idx = 0;
-
-	char o_name[80];
-
-
-	/* Scan the pile of objects */
-	for (this_o_idx = c_ptr->o_idx; this_o_idx; this_o_idx = next_o_idx)
-	{
-		/* Acquire object */
-		object_type *o_ptr = &o_list[this_o_idx];
-
-		/* Acquire next object */
-		next_o_idx = o_ptr->next_o_idx;
-
-		/* Known to be worthless? */
-		if (destroy_worthless && (object_value(o_ptr) < 1))
-		{
-			/* Artifact? */
-			if (!can_player_destroy_object(o_ptr))
-			{
-				/* Describe the object (with {terrible/special}) */
-				object_desc(o_name, o_ptr, TRUE, 3);
-
-				/* Message */
-				msg_format("You cannot auto-destroy %s.", o_name);
-
-				/* Done */
-				return;
-			}
-
-			/* Describe the object */
-			object_desc(o_name, o_ptr, TRUE, 3);
-
-			/* Print a message */
-			msg_format("Auto-destroying %s.", o_name);
-
-			/* Destroy the item */
-			delete_object_idx(this_o_idx);
-
-			continue;
-		}
-	}
-}
-
-#endif /* 0 */
-
-
 /*
  * Player "wants" to pick up an object or gold.
  * Note that we ONLY handle things that can be picked up.
@@ -776,8 +722,6 @@ void carry(int pickup)
 	int floor_num = 0, floor_list[23], floor_o_idx = 0;
 
 	int can_pickup = 0;
-
-	bool do_ask = TRUE;
 	
 	/* Recenter the map around the player */
 	verify_panel();
@@ -793,17 +737,6 @@ void carry(int pickup)
 	
 	/* Handle stuff */
 	handle_stuff();
-
-#if 0	
-	/*
-	 * This has been disabled - the object distribution has
-	 * changed to remove the large amount of junk.
-	 */
-	
-	/* Destroy worthless items below the player */
-	auto_destroy_items(area(py, px));
-
-#endif /* 0 */
 
 	/* Scan the pile of objects */
 	for (this_o_idx = area(py, px)->o_idx; this_o_idx; this_o_idx = next_o_idx)
@@ -1007,37 +940,61 @@ void carry(int pickup)
 	/* One object */
 	if (floor_num == 1)
 	{
+		/* Remember the object to pick up */
+		this_o_idx = floor_o_idx;
+		
+		/* Access the object */
+		o_ptr = &o_list[this_o_idx];
+		
 		/* Hack -- query every object */
 		if (carry_query_flag)
 		{
+			int i;
 			char out_val[160];
+					
+			/* Paranoia XXX XXX XXX */
+			msg_print(NULL);
+					
+			sprintf(out_val, "Pick up %s? [y/n/k] ", o_name);
+					
+			/* Prompt for it */
+			prt(out_val, 0, 0);
 
-			/* Access the object */
-			o_ptr = &o_list[floor_o_idx];
-
-			/* Describe the object */
-			object_desc(o_name, o_ptr, TRUE, 3);
-
-			/* Build a prompt */
-			(void)sprintf(out_val, "Pick up %s? ", o_name);
-
-			/* Ask the user to confirm */
-			if (!get_check(out_val))
+			/* Get an acceptable answer */
+			while (TRUE)
 			{
-				/* Done */
-				return;
+				i = inkey();
+				if (quick_messages) break;
+				if (i == ESCAPE) break;
+				if (strchr("YyNnKk", i)) break;
+				bell();
 			}
+
+			/* Erase the prompt */
+			prt("", 0, 0);
+					
+			if ((i == 'Y') || (i == 'y'))
+			{
+				/* Pick up the object */
+				py_pickup_aux(this_o_idx);
+			}
+					
+			if ((i == 'K') || (i == 'k'))
+			{
+				/* Physically try to destroy the item */
+				if	(destroy_item_aux(o_ptr, o_ptr->number))
+				{
+					delete_object_idx(this_o_idx);
+				}
+			}
+
+			/* Done */					
+			return;
 		}
-
-		/* Don't ask */
-		do_ask = FALSE;
-
-		/* Remember the object to pick up */
-		this_o_idx = floor_o_idx;
 	}
 
 	/* Allow the user to choose an object */
-	if (do_ask)
+	else
 	{
 		cptr q, s;
 
@@ -3175,18 +3132,7 @@ static bool run_test(void)
 					/* Done */
 					break;
 				}
-#if 0
-				/* quest features */
-				case FEAT_QUEST_ENTER:
-				case FEAT_QUEST_EXIT:
-				{
-					/* Notice */
-					notice = TRUE;
 
-					/* Done */
-					break;
-				}
-#endif
 				case FEAT_DEEP_LAVA:
 				case FEAT_SHAL_LAVA:
 				{
