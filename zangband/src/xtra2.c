38952dbb9253d7493efb63ffd59f0f130f710481
rr9 <>
2001-04-22 10:30:57 +0000

    The 'Yellow light' can now blind you when exploding.

--- zangband/src/xtra2.c
+++ zangband/src/xtra2.c
@@ -279,7 +279,7 @@ bool monster_death(int m_idx)
 				case RBE_ELEC:      typ = GF_ELEC; break;
 				case RBE_FIRE:      typ = GF_FIRE; break;
 				case RBE_COLD:      typ = GF_COLD; break;
-				case RBE_BLIND:     typ = GF_MISSILE; break;
+				case RBE_BLIND:     typ = GF_LITE; break; /* HACK - for Yellow light */
 				case RBE_CONFUSE:   typ = GF_CONFUSION; break;
 				case RBE_TERRIFY:   typ = GF_MISSILE; break;
 				case RBE_PARALYZE:  typ = GF_MISSILE; break;
