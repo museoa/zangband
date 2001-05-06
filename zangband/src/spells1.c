a487748aa62614a4ea50f6456a38212bd14d3415
sfuerst <>
2001-05-06 10:59:51 +0000

    Minor change to the message you get when you spot a mimic.

--- zangband/src/spells1.c
+++ zangband/src/spells1.c
@@ -2872,7 +2872,7 @@ static bool project_m(int who, int r, int y, int x, int dam, int typ)
 		update_mon_vis(m_ptr->r_idx, 1);
 		
 		/* We've spotted it */
-		msg_format("You discover %s!", m_name);
+		msg_format("You've found %s!", m_name);
 	}
 
 
