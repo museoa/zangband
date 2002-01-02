b5767f274726b528083758310c88629145043e8b
sfuerst <>
2002-01-02 16:51:13 +0000

    Quest rarities were reversed.

--- zangband/src/quest.c
+++ zangband/src/quest.c
@@ -1346,11 +1346,11 @@ static int pick_quest_type(quest_aux_type *l_ptr, int level)
 
 static quest_aux_type camp_types[] =
 {
-	{quest_aux_orc,		10,	1, "orc"},
-	{quest_aux_undead,	20,	4, "undead"},
-	{quest_aux_troll,	40,	1, "troll"},
-	{quest_aux_giant,	60,	2, "giant"},
-	{quest_aux_dragon,	80,	4, "dragon"},
+	{quest_aux_undead,	10,	1, "undead"},
+	{quest_aux_orc,		20,	2, "orc"},
+	{quest_aux_troll,	40,	4, "troll"},
+	{quest_aux_giant,	60,	1, "giant"},
+	{quest_aux_dragon,	80,	1, "dragon"},
 	{NULL,				0,	0, NULL},
 };
 
