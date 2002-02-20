e87ef62e19af747d8b209e2ca3b82a60876abeb5
sfuerst <>
2002-02-20 20:48:22 +0000

    Added missing cast that caused problems on 16bit machines.

--- zangband/src/main-xpj.c
+++ zangband/src/main-xpj.c
@@ -66,7 +66,7 @@
 
 
 
-/* Rest of the dependancies */
+/* Rest of the dependencies */
 
 #ifndef __MAKEDEPEND__
 #include <X11/Xlib.h>
@@ -774,7 +774,7 @@ static errr Metadpy_init_2(Display *dpy, cptr name)
 	m->fg = m->white;
 
 	/* Calculate the Maximum allowed Pixel value.  */
-	m->zg = (1 << m->depth) - 1;
+	m->zg = ((Pixell)1 << m->depth) - 1;
 
 	/* Save various default Flag Settings */
 	m->color = ((m->depth > 1) ? 1 : 0);
