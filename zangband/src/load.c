2262270611a468302653dd812d953a907c6a7a30
sfuerst <>
2002-03-01 21:11:25 +0000

    Fixed silly bug caused by not storing the old player inventory properly when loading old savefiles.

--- zangband/src/load.c
+++ zangband/src/load.c
@@ -319,6 +319,17 @@ static void rd_item(object_type *o_ptr)
 		rd_s16b(&o_ptr->iy);
 		rd_s16b(&o_ptr->ix);
 	}
+	
+	
+	if (sf_version < 37)
+	{
+		/* Mega - hack import player inventory properly */
+		if (!o_ptr->ix && !o_ptr->iy)
+		{
+			o_ptr->held = TRUE;
+		}
+	}
+	
 	/* Type/Subtype */
 	rd_byte(&o_ptr->tval);
 	rd_byte(&o_ptr->sval);
