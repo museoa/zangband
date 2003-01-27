607aff96298d1d33a4cb6f2262de2f87bc2b85b3
antimatter <>
2003-01-27 06:03:38 +0000

    Add the label for returning on the wizard object flags screen. Oops.

--- zangband/src/wizard2.c
+++ zangband/src/wizard2.c
@@ -630,11 +630,11 @@ static void wiz_display_item(const object_type *o_ptr)
                     "%v", binary_fmt, o_ptr->flags3);
 
 	prtf(j + 32, 17,"+------------FLAGS4-------------\n"
-			"        IMSH p pt   HURT..  CURS\n"
-			"        ldac alao   aefcld  as h\n"
-			"        iacomtusup  clioia  utee\n"
-			"        trilurcscs  ierltr  taaa\n"
-			"        ekddtnkwhi  dcedek  ottl\n"
+			"        IMSH p pt r HURT..  CURS\n"
+			"        ldac alao e aefcld  as h\n"
+			"        iacomtusupt clioia  utee\n"
+			"        trilurcscsr ierltr  taaa\n"
+			"        ekddtnkwhin dcedek  ottl\n"
 		    "%v", binary_fmt, o_ptr->flags4);
 
 }
