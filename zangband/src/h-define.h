09c2360d2d2ea70a33c82828cb1478729bcb28fc
sfuerst <>
2003-04-13 20:34:25 +0000

    Fix stupidity on my behalf.

--- zangband/src/h-define.h
+++ zangband/src/h-define.h
@@ -163,16 +163,16 @@
 #ifdef VM
 #  define A2I(X)	alphatoindex(X)
 #  define I2A(X)	indextoalpha(X)
-#  define D2I(X)	((char) ((X) - '0'))
+#  define D2I(X)	((X) - '0')
 #  define I2D(X)	((char) ((X) + '0'))
-#  define KTRL(X)	((char) ((X) & 0x1F))
+#  define KTRL(X)	((X) & 0x1F)
 #  define ESCAPE	'\033'
 #else
-#  define A2I(X)	((char) ((X) - 'a'))
+#  define A2I(X)	((X) - 'a')
 #  define I2A(X)	((char) ((X) + 'a'))
-#  define D2I(X)	((char) ((X) - '0'))
+#  define D2I(X)	((X) - '0')
 #  define I2D(X)	((char) ((X) + '0'))
-#  define KTRL(X)	((char) ((X) & 0x1F))
+#  define KTRL(X)	((X) & 0x1F)
 #  define ESCAPE	'\033'
 #endif
 
